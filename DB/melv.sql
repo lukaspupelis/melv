5d058d4c7d434de9b9fe8b24ce125b434c51995a
Lukas <pupelislukas053@gmail.com>
2020-06-01 22:35:04 +0300

    periodinis trynimas

--- DB/melv.sql
+++ DB/melv.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.9.1
+-- version 5.0.1
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: 2020 m. Geg 23 d. 21:47
--- Server version: 10.4.8-MariaDB
--- PHP Version: 7.3.11
+-- Generation Time: Jun 01, 2020 at 09:33 PM
+-- Server version: 10.4.11-MariaDB
+-- PHP Version: 7.2.28
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
@@ -25,7 +25,7 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Sukurta duomenų struktūra lentelei `administrators`
+-- Table structure for table `administrators`
 --
 
 CREATE TABLE `administrators` (
@@ -33,7 +33,7 @@ CREATE TABLE `administrators` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
--- Sukurta duomenų kopija lentelei `administrators`
+-- Dumping data for table `administrators`
 --
 
 INSERT INTO `administrators` (`fk_user`) VALUES
@@ -42,7 +42,7 @@ INSERT INTO `administrators` (`fk_user`) VALUES
 -- --------------------------------------------------------
 
 --
--- Sukurta duomenų struktūra lentelei `clients`
+-- Table structure for table `clients`
 --
 
 CREATE TABLE `clients` (
@@ -52,7 +52,7 @@ CREATE TABLE `clients` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
--- Sukurta duomenų kopija lentelei `clients`
+-- Dumping data for table `clients`
 --
 
 INSERT INTO `clients` (`fk_user`, `birthdate`, `personal_no`) VALUES
@@ -71,7 +71,7 @@ INSERT INTO `clients` (`fk_user`, `birthdate`, `personal_no`) VALUES
 -- --------------------------------------------------------
 
 --
--- Sukurta duomenų struktūra lentelei `entertainment_plans`
+-- Table structure for table `entertainment_plans`
 --
 
 CREATE TABLE `entertainment_plans` (
@@ -84,7 +84,7 @@ CREATE TABLE `entertainment_plans` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
--- Sukurta duomenų kopija lentelei `entertainment_plans`
+-- Dumping data for table `entertainment_plans`
 --
 
 INSERT INTO `entertainment_plans` (`id`, `title`, `text`, `price`, `removed`, `fk_administrator`) VALUES
@@ -93,7 +93,7 @@ INSERT INTO `entertainment_plans` (`id`, `title`, `text`, `price`, `removed`, `f
 -- --------------------------------------------------------
 
 --
--- Sukurta duomenų struktūra lentelei `flights`
+-- Table structure for table `flights`
 --
 
 CREATE TABLE `flights` (
@@ -106,19 +106,21 @@ CREATE TABLE `flights` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
--- Sukurta duomenų kopija lentelei `flights`
+-- Dumping data for table `flights`
 --
 
 INSERT INTO `flights` (`id`, `direction`, `departure_date`, `arrival_date`, `confirmed`, `fk_administrator`) VALUES
 (6, 0, '2020-05-13', '2020-06-12', 0, 2),
 (7, 0, '2020-05-01', '2020-05-31', 0, 2),
 (8, 0, '2020-05-30', '2020-06-29', 0, 2),
-(9, 0, '2020-05-25', '2020-06-24', 0, 2);
+(9, 0, '2020-05-25', '2020-06-24', 0, 2),
+(10, 0, '2020-05-06', '2020-06-05', 0, 2),
+(11, 1, '2020-05-02', '2020-06-01', 0, 2);
 
 -- --------------------------------------------------------
 
 --
--- Sukurta duomenų struktūra lentelei `flight_durations`
+-- Table structure for table `flight_durations`
 --
 
 CREATE TABLE `flight_durations` (
@@ -129,10 +131,39 @@ CREATE TABLE `flight_durations` (
   `price` decimal(10,2) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
+--
+-- Dumping data for table `flight_durations`
+--
+
+INSERT INTO `flight_durations` (`id`, `date_mars`, `date_earth`, `direction`, `price`) VALUES
+(1, '2021-03-20', '2020-05-12', 0, '370200.00'),
+(2, '2021-03-19', '2020-05-11', 0, '370200.00'),
+(3, '2021-03-05', '2020-05-04', 0, '368625.00'),
+(4, '2021-03-22', '2020-05-13', 0, '370425.00'),
+(5, '2021-04-18', '2020-05-27', 0, '373350.00'),
+(6, '2021-04-05', '2020-05-20', 0, '372000.00'),
+(7, '2021-04-01', '2020-05-18', 0, '371550.00'),
+(8, '2021-04-03', '2020-05-19', 0, '371775.00'),
+(9, '2021-04-20', '2020-05-28', 0, '373575.00'),
+(10, '2021-04-14', '2020-05-25', 0, '372900.00'),
+(11, '2021-04-16', '2020-05-26', 0, '373125.00'),
+(12, '2020-05-11', '2020-07-08', 1, '313050.00'),
+(13, '2020-05-12', '2020-07-09', 1, '313050.00'),
+(14, '2020-05-27', '2021-02-17', 1, '359850.00'),
+(15, '2020-05-21', '2021-02-14', 1, '360525.00'),
+(16, '2020-05-26', '2021-02-16', 1, '359850.00'),
+(17, '2021-04-21', '2020-05-29', 0, '373575.00'),
+(18, '2021-04-06', '2020-05-21', 0, '372000.00'),
+(19, '2021-03-26', '2020-05-15', 0, '370875.00'),
+(20, '2020-05-20', '2021-02-13', 1, '360525.00'),
+(21, '2020-05-14', '2020-07-12', 1, '313275.00'),
+(22, '2020-05-19', '2021-02-13', 1, '360750.00'),
+(23, '2020-05-18', '2021-02-12', 1, '360750.00');
+
 -- --------------------------------------------------------
 
 --
--- Sukurta duomenų struktūra lentelei `flight_requests`
+-- Table structure for table `flight_requests`
 --
 
 CREATE TABLE `flight_requests` (
@@ -149,49 +180,39 @@ CREATE TABLE `flight_requests` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
--- Sukurta duomenų kopija lentelei `flight_requests`
+-- Dumping data for table `flight_requests`
 --
 
 INSERT INTO `flight_requests` (`id`, `direction`, `departure_date`, `arrival_date`, `price`, `fk_flight`, `fk_entertainment_plan`, `fk_food_plan`, `fk_sport_plan`, `fk_client`) VALUES
-(2, 0, '2020-05-13', '2020-05-22', '100000.00', NULL, 1, 22, 1, 1),
-(3, 0, '2020-05-10', '2020-05-22', '1000000.00', NULL, 1, 27, 1, 3),
-(4, 0, '2020-05-08', '2020-05-18', '10000000.00', NULL, 1, 24, 1, 4),
-(5, 0, '2020-05-19', '2020-05-30', '1000000.00', NULL, 1, 21, 1, 5),
-(6, 0, '2020-05-14', '2020-05-25', '10000000.00', NULL, 1, 25, 1, 6),
-(7, 0, '2020-05-15', '2020-05-25', '10000000.00', NULL, 1, 22, 1, 7),
-(8, 0, '2020-05-13', '2020-05-25', '10000000.00', NULL, 1, 24, 1, 8),
-(9, 1, '2020-05-30', '2020-06-10', '1000000.00', NULL, 1, 24, 1, 9),
-(10, 0, '2020-05-02', '2020-05-19', '10000.00', NULL, 1, 25, 1, 10),
-(11, 1, '2020-05-20', '2020-05-29', '100000.00', NULL, 1, 25, 1, 11),
-(12, 1, '2020-05-11', '2020-05-20', '1000000.00', NULL, 1, 24, 1, 12),
-(13, 0, '2020-05-13', '2020-05-20', '100000.00', NULL, 1, 28, 1, 3),
-(14, 1, '2020-05-20', '2020-05-27', '10000.00', NULL, 1, 26, 1, 4),
-(15, 0, '2020-06-18', '2020-05-29', '1000000.00', NULL, 1, 22, 1, 6),
-(16, 0, '2020-05-10', '2020-05-20', '1000000.00', NULL, 1, 28, 1, 3),
-(17, 0, '2020-05-30', '2020-06-18', '100000.00', NULL, 1, 21, 1, 12),
-(18, 0, '2020-05-30', '2020-06-18', '100000.00', NULL, 1, 24, 1, 11),
-(19, 0, '2020-05-30', '2020-06-18', '100000.00', NULL, 1, 28, 1, 10),
-(20, 0, '2020-05-30', '2020-06-18', '1000000.00', NULL, 1, 21, 1, 8),
-(21, 0, '2020-05-30', '2020-06-18', '1000000.00', NULL, 1, 23, 1, 7),
-(22, 0, '2020-05-30', '2020-06-18', '100000.00', NULL, 1, 21, 1, 6),
-(23, 0, '2020-05-30', '2020-06-18', '1000000.00', NULL, 1, 24, 1, 5),
-(24, 0, '2020-05-30', '2020-06-18', '100000.00', NULL, 1, 21, 1, 4),
-(25, 0, '2020-05-30', '2020-05-28', '100000.00', NULL, 1, 28, 1, 1),
-(26, 0, '2020-05-01', '2020-05-14', '1000000.00', NULL, 1, 21, 1, 3),
-(27, 0, '2020-05-01', '2020-05-14', '100000.00', NULL, 1, 24, 1, 4),
-(28, 0, '2020-05-01', '2020-05-13', '100000.00', NULL, 1, 26, 1, 5),
-(29, 0, '2020-05-01', '2020-05-15', '10000.00', NULL, 1, 26, 1, 6),
-(30, 0, '2020-05-01', '2020-05-08', '100000.00', NULL, 1, 21, 1, 7),
-(31, 0, '2020-05-01', '2020-05-13', '100000.00', NULL, 1, 25, 1, 8),
-(32, 0, '2020-05-01', '2020-05-19', '100000.00', NULL, 1, 25, 1, 9),
-(33, 0, '2020-05-01', '2020-05-15', '1000000.00', NULL, 1, 24, 1, 10),
-(34, 0, '2020-05-01', '2020-05-21', '100000.00', NULL, 1, 28, 1, 11),
-(35, 0, '2020-05-01', '2020-05-20', '100000.00', NULL, 1, 25, 1, 12);
+(2, 0, '2020-06-13', '2020-06-22', '100000.00', NULL, 1, 22, 1, 1),
+(3, 0, '2020-06-10', '2020-06-22', '1000000.00', NULL, 1, 27, 1, 3),
+(4, 0, '2020-06-08', '2020-06-18', '10000000.00', NULL, 1, 24, 1, 4),
+(5, 0, '2020-06-19', '2020-06-30', '1000000.00', NULL, 1, 21, 1, 5),
+(6, 0, '2020-06-14', '2020-06-25', '10000000.00', NULL, 1, 25, 1, 6),
+(7, 0, '2020-06-15', '2020-06-25', '10000000.00', NULL, 1, 22, 1, 7),
+(8, 0, '2020-06-13', '2020-06-25', '10000000.00', NULL, 1, 24, 1, 8),
+(9, 0, '2020-06-30', '2020-07-10', '1000000.00', NULL, 1, 24, 1, 9),
+(10, 0, '2020-06-02', '2020-06-19', '10000.00', NULL, 1, 25, 1, 10),
+(11, 1, '2020-06-20', '2020-06-29', '100000.00', NULL, 1, 25, 1, 11),
+(12, 1, '2020-06-11', '2020-06-20', '1000000.00', NULL, 1, 24, 1, 12),
+(13, 0, '2020-06-13', '2020-06-20', '100000.00', NULL, 1, 28, 1, 3),
+(14, 1, '2020-06-20', '2020-06-27', '10000.00', NULL, 1, 26, 1, 4),
+(15, 0, '2020-07-18', '2020-07-29', '1000000.00', NULL, 1, 22, 1, 6),
+(16, 0, '2020-06-10', '2020-06-20', '1000000.00', NULL, 1, 28, 1, 3),
+(17, 0, '2020-06-30', '2020-07-18', '100000.00', NULL, 1, 21, 1, 12),
+(18, 0, '2020-06-30', '2020-07-18', '100000.00', NULL, 1, 24, 1, 11),
+(19, 0, '2020-06-30', '2020-07-18', '100000.00', NULL, 1, 28, 1, 10),
+(20, 0, '2020-06-30', '2020-07-18', '1000000.00', NULL, 1, 21, 1, 8),
+(21, 0, '2020-06-30', '2020-07-18', '1000000.00', NULL, 1, 23, 1, 7),
+(22, 0, '2020-06-30', '2020-07-18', '100000.00', NULL, 1, 21, 1, 6),
+(23, 0, '2020-06-30', '2020-07-18', '1000000.00', NULL, 1, 24, 1, 5),
+(24, 0, '2020-06-30', '2020-07-18', '100000.00', NULL, 1, 21, 1, 4),
+(25, 0, '2020-06-30', '2020-07-28', '100000.00', NULL, 1, 28, 1, 1);
 
 -- --------------------------------------------------------
 
 --
--- Sukurta duomenų struktūra lentelei `food_plans`
+-- Table structure for table `food_plans`
 --
 
 CREATE TABLE `food_plans` (
@@ -204,7 +225,7 @@ CREATE TABLE `food_plans` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
--- Sukurta duomenų kopija lentelei `food_plans`
+-- Dumping data for table `food_plans`
 --
 
 INSERT INTO `food_plans` (`id`, `title`, `text`, `price`, `removed`, `fk_administrator`) VALUES
@@ -222,7 +243,7 @@ INSERT INTO `food_plans` (`id`, `title`, `text`, `price`, `removed`, `fk_adminis
 -- --------------------------------------------------------
 
 --
--- Sukurta duomenų struktūra lentelei `payments`
+-- Table structure for table `payments`
 --
 
 CREATE TABLE `payments` (
@@ -237,7 +258,7 @@ CREATE TABLE `payments` (
 -- --------------------------------------------------------
 
 --
--- Sukurta duomenų struktūra lentelei `sport_plans`
+-- Table structure for table `sport_plans`
 --
 
 CREATE TABLE `sport_plans` (
@@ -250,7 +271,7 @@ CREATE TABLE `sport_plans` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
--- Sukurta duomenų kopija lentelei `sport_plans`
+-- Dumping data for table `sport_plans`
 --
 
 INSERT INTO `sport_plans` (`id`, `title`, `text`, `price`, `removed`, `fk_administrator`) VALUES
@@ -259,7 +280,7 @@ INSERT INTO `sport_plans` (`id`, `title`, `text`, `price`, `removed`, `fk_admini
 -- --------------------------------------------------------
 
 --
--- Sukurta duomenų struktūra lentelei `users`
+-- Table structure for table `users`
 --
 
 CREATE TABLE `users` (
@@ -273,7 +294,7 @@ CREATE TABLE `users` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
--- Sukurta duomenų kopija lentelei `users`
+-- Dumping data for table `users`
 --
 
 INSERT INTO `users` (`id`, `name`, `last_name`, `email`, `password`, `phone`, `removed`) VALUES
@@ -379,19 +400,19 @@ ALTER TABLE `entertainment_plans`
 -- AUTO_INCREMENT for table `flights`
 --
 ALTER TABLE `flights`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
 
 --
 -- AUTO_INCREMENT for table `flight_durations`
 --
 ALTER TABLE `flight_durations`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=24;
 
 --
 -- AUTO_INCREMENT for table `flight_requests`
 --
 ALTER TABLE `flight_requests`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=36;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=39;
 
 --
 -- AUTO_INCREMENT for table `food_plans`
@@ -418,35 +439,35 @@ ALTER TABLE `users`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
 
 --
--- Apribojimai eksportuotom lentelėm
+-- Constraints for dumped tables
 --
 
 --
--- Apribojimai lentelei `administrators`
+-- Constraints for table `administrators`
 --
 ALTER TABLE `administrators`
   ADD CONSTRAINT `fk_user` FOREIGN KEY (`fk_user`) REFERENCES `users` (`id`);
 
 --
--- Apribojimai lentelei `clients`
+-- Constraints for table `clients`
 --
 ALTER TABLE `clients`
   ADD CONSTRAINT `fk_user1` FOREIGN KEY (`fk_user`) REFERENCES `users` (`id`);
 
 --
--- Apribojimai lentelei `entertainment_plans`
+-- Constraints for table `entertainment_plans`
 --
 ALTER TABLE `entertainment_plans`
   ADD CONSTRAINT `fk_eplancreator` FOREIGN KEY (`fk_administrator`) REFERENCES `administrators` (`fk_user`);
 
 --
--- Apribojimai lentelei `flights`
+-- Constraints for table `flights`
 --
 ALTER TABLE `flights`
   ADD CONSTRAINT `fk_registered` FOREIGN KEY (`fk_administrator`) REFERENCES `administrators` (`fk_user`);
 
 --
--- Apribojimai lentelei `flight_requests`
+-- Constraints for table `flight_requests`
 --
 ALTER TABLE `flight_requests`
   ADD CONSTRAINT `fk_client` FOREIGN KEY (`fk_client`) REFERENCES `clients` (`fk_user`),
@@ -456,23 +477,31 @@ ALTER TABLE `flight_requests`
   ADD CONSTRAINT `fk_splan` FOREIGN KEY (`fk_sport_plan`) REFERENCES `sport_plans` (`id`);
 
 --
--- Apribojimai lentelei `food_plans`
+-- Constraints for table `food_plans`
 --
 ALTER TABLE `food_plans`
   ADD CONSTRAINT `fk_fplancreator` FOREIGN KEY (`fk_administrator`) REFERENCES `administrators` (`fk_user`);
 
 --
--- Apribojimai lentelei `payments`
+-- Constraints for table `payments`
 --
 ALTER TABLE `payments`
   ADD CONSTRAINT `fk_payer` FOREIGN KEY (`fk_client`) REFERENCES `clients` (`fk_user`),
   ADD CONSTRAINT `fk_pflight` FOREIGN KEY (`fk_flight`) REFERENCES `flights` (`id`);
 
 --
--- Apribojimai lentelei `sport_plans`
+-- Constraints for table `sport_plans`
 --
 ALTER TABLE `sport_plans`
   ADD CONSTRAINT `fk_splancreator` FOREIGN KEY (`fk_administrator`) REFERENCES `administrators` (`fk_user`);
+
+DELIMITER $$
+--
+-- Events
+--
+CREATE DEFINER=`root`@`localhost` EVENT `delete_flight_requests` ON SCHEDULE EVERY 1 MONTH STARTS '2020-06-01 22:29:01' ON COMPLETION PRESERVE ENABLE DO delete from flight_requests where now() > departure_date$$
+
+DELIMITER ;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
