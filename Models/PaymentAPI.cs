0ba250df1f4dbfb2bd65249ed05888e6b005655f
Lukas <pupelislukas053@gmail.com>
2020-06-02 21:52:30 +0300

    main page

---
+++ Models/PaymentAPI.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using MySql.Data.MySqlClient;
+
+namespace MELV_IS.Models
+{
+    public class PaymentAPI
+    {
+
+    }
+}
